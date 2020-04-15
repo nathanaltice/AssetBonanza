977283d42804dc34896423155fb3ba1962dfd1a7
Nathan Altice <naltice@ucsc.edu>
2020-04-14 19:40:42 -0700

    more asset types added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 // Asset Management
 // Testing various asset types
 
+// discipline
+'use strict';
+
 // Test Scene
 class Test extends Phaser.Scene {
     constructor() {
@@ -10,7 +13,7 @@ class Test extends Phaser.Scene {
     }
 
     preload() {
-        // set load path
+        // set load path to save some typing
         this.load.path = "./assets/";
         // images
         this.load.image('colorsquare', 'colorsquare.png');
@@ -18,26 +21,31 @@ class Test extends Phaser.Scene {
         this.load.image('parallaxSky', 'parallaxSky.png');
         this.load.image('parallaxMountains', 'parallaxMountains.png');
         this.load.image('parallaxTreeline', 'parallaxTreeline.png');
+        this.load.image('squarepattern', 'squarepattern.png');
         // spritesheet
         this.load.spritesheet('pinkhover', 'pinkhover.png', {
             frameWidth: 100,
             frameHeight: 100,
             endFrame: 7
         });
+        // texture atlas aka fruit salad time 🍇
+        this.load.atlas('fruitandveg', 'fruitandveg.png', 'fruitandveg.json');
+        // sound
+        this.load.audio('jumpSFX', 'jumpSFX.wav');
     }
 
     create() {
         // demonstrate images
-        // first
+        // first (static)
         this.colorsquare00 = this.add.image(0, 0, 'colorsquare').setOrigin(0,0);
-        // second
+        // second (flip Y)
         this.colorsquare01 = this.add.image(100, 0, 'colorsquare').setOrigin(0,0);
         this.colorsquare01.setAlpha(0.1, 0.5, 1, 0.75);
         this.colorsquare01.flipY = true;  
-        // third
+        // third (tint - Gameboy-esque colors 💚)
         this.colorsquare02 = this.add.image(200, 0, 'colorsquare').setOrigin(0,0);
-        this.colorsquare02.tint = 0x00AA11;         // WebGL only (not Canvas)
-        // fourth
+        this.colorsquare02.tint = 0x00FF11;         // WebGL only
+        // fourth (interactive Y flip / blend mode)
         this.colorsquare03 = this.add.image(300, 0, 'colorsquare').setOrigin(0,0);
         this.colorsquare03.flipY = true;
         this.colorsquare03.blendMode = 'SCREEN';    // WebGL only
@@ -45,23 +53,25 @@ class Test extends Phaser.Scene {
             console.log(`pointer: ${pointer}, localX: ${localX}, localY: ${localY}, event: ${event}`);
             this.colorsquare03.toggleFlipY();   // context is still the scene
         });
-        // fifth
+        // fifth (pointerover / change texture / set random position / play sound)
         this.colorsquare04 = this.add.image(400, 0, 'colorsquare').setOrigin(0,0);
-        this.colorsquare04.setInteractive().on('pointerdown',()=>{
+        this.colorsquare04.setInteractive().on('pointerover',()=>{
             this.colorsquare04.setTexture('fastboy');
             this.colorsquare04.setRandomPosition();
+            this.colorsquare04.setDepth(10);
+            this.sound.play('jumpSFX');
         });
-        // sixth
+        // sixth (interactive angle change)
         this.colorsquare05 = this.add.image(550, 50, 'colorsquare');
         this.colorsquare05.setInteractive().on('pointerdown',()=>{
             this.colorsquare05.angle += 45;
         });
-        // seventh
+        // seventh (interactive make smol)
         this.colorsquare06 = this.add.image(600, 0, 'colorsquare').setOrigin(0,0);
         this.colorsquare06.setInteractive().on('pointerdown',()=>{
             this.colorsquare06.setScale(0.25);
         });
-        // eighth
+        // eighth (display depth / interactive tween)
         this.colorsquare07 = this.add.image(700, 0, 'colorsquare').setOrigin(0,0);
         this.colorsquare07.setDepth(-1);    // set depth below other squares
         this.colorsquare07.setInteractive().on('pointerdown',()=>{
@@ -76,9 +86,9 @@ class Test extends Phaser.Scene {
         });
 
         // demonstrate sprites
-        // static w/ specific frame number
+        // first (static w/ specific frame number)
         this.pinkhoverStatic = this.add.sprite(100, 200, 'pinkhover', 3);
-        // animated, looping
+        // second (animated / infinite repeat)
         let config = {
             key: 'hoverAnimation',
             frames: this.anims.generateFrameNumbers('pinkhover', { start: 0, end: 7, first: 0}),
@@ -88,11 +98,35 @@ class Test extends Phaser.Scene {
         this.anims.create(config);
         this.pinkhoverAnimated = this.add.sprite(200, 200, 'pinkhover').play('hoverAnimation');
 
-        // demonstrate
+        // demonstrate tile sprites x3 for parallax effect
         this.skyline = this.add.tileSprite(0, 455, 800, 100, 'parallaxSky').setOrigin(0,0);
         this.mountains = this.add.tileSprite(0, 455, 800, 100, 'parallaxMountains').setOrigin(0,0);
         this.treeline = this.add.tileSprite(0, 500, 800, 100, 'parallaxTreeline').setOrigin(0,0);
 
+        // demonstrate quad (tween alpha / corners)
+        this.add.quad(400, 300, 'squarepattern');
+        this.checkers = this.add.quad(600, 300, 'squarepattern');
+        this.checkers.setDepth(-2);
+        this.tweens.add({
+            targets: this.checkers,
+            bottomRightAlpha: 0.5,
+            topLeftX: 700,
+            topLeftY: 500,
+            topRightX: 650,
+            topRightY: 15,
+            bottomRightX: 800,
+            bottomRightY: 310,
+            bottomLeftX: 300,
+            bottomLeftY: 400,
+            duration: 10000,
+            yoyo: true,
+            ease: 'Linear',
+            repeat: -1,
+        });
+
+        // add sprites from our texture atlas
+        this.fruit01 = this.add.sprite(100, 400, 'fruitandveg', 'pear');
+        this.fruit02 = this.add.sprite(200, 400, 'fruitandveg', 'tomato');
     }
 
     update() {
@@ -115,3 +149,9 @@ let config = {
 }
 
 let game = new Phaser.Game(config);
+
+// load texture atlas
+this.load.atlas('key', 'image.png', 'atlas.json');
+
+// add a texture atlas sprite
+this.add.sprite(x, y, 'key', 'frame_name');
